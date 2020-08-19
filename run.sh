d7dfbfefdb214a23967657a91b9dd600f9f5c8c0
Feras.alhassnan <34692847+FerasAlhassnan@users.noreply.github.com>
2020-08-19 12:55:37 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CIERA_VERSION=2.1.1-SNAPSHOT
-CLASSPATH=$HOME/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$HOME/.m2/repository/org/json/json/20180813/json-20180813.jar:$HOME/.m2/repository/io/ciera/Deployment/1.0.0-SNAPSHOT/Deployment-1.0.0-SNAPSHOT.jar
-java -cp $CLASSPATH deployment.DeploymentApplication $@
